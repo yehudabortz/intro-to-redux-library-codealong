a7458838aa8ebee19211058f070c2507dde9ac3a
Yehuda Bortz <yehudabortz@icloud.com>
2021-04-21 19:16:47 -0500

    commit tree e1090c284fe384fa35741dbb2e122912bceeb540

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 // ./src/index.js
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux'; /* code change */
-import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux"; /* code change */
+import shoppingListItemReducer from "./reducers/shoppingListItemReducer.js";
+import App from "./App";
+import "./index.css";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(shoppingListItemReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
